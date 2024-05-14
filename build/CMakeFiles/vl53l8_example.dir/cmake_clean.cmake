88d1b752fc4407762363bda6e87bc68a4fc044b4
chenjiasheng <xdr_jiasheng@sjtu.edu.cn>
2024-05-14 15:05:55 +0800

    v0.5

---
+++ build/CMakeFiles/vl53l8_example.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vl53l8_example.dir/main.cpp.o"
+  "CMakeFiles/vl53l8_example.dir/vl53l8_oper.cpp.o"
+  "vl53l8_example"
+  "vl53l8_example.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vl53l8_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
